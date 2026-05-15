713d13276983bbef89cd9efb92be9429bee90b97
Jacob <namelomax@gmail.com>
2026-05-15 12:05:46 +0800

    Rag Fix

--- rag-api/rag_service.py
+++ rag-api/rag_service.py
@@ -392,7 +392,7 @@ class RAGService:
             lightrag_kwargs={
                 "llm_model_kwargs": {"timeout": 6000},
                 "llm_model_max_async": 1,
-                "chunk_token_size": 200,
+                "chunk_token_size": 400,
                 "chunk_overlap_token_size": 80,
                 "vector_db_storage_cls_kwargs": {"cosine_better_than_threshold": 0.1},
                 "addon_params": {"language": "Russian"},
